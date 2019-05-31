3f6f6a2a2378af55afbb5fed138cc4b21d5f057b
Andrew Tridgell <andrew@tridgell.net>
2019-05-31 20:58:27 +1000

    GCS_MAVLink: added handle_command_landing_target()
    used to allow vehicle handling of time corrected LANDING_TARGET msgs

--- libraries/GCS_MAVLink/GCS.h
+++ libraries/GCS_MAVLink/GCS.h
@@ -619,6 +619,9 @@ protected:
     MAV_RESULT handle_command_get_home_position(const mavlink_command_long_t &packet);
     MAV_RESULT handle_command_do_fence_enable(const mavlink_command_long_t &packet);
 
+    // default empty handling of LANDING_TARGET
+    virtual MAV_RESULT handle_command_landing_target(const mavlink_landing_target_t &packet, uint32_t timestamp_ms) { return MAV_RESULT_UNSUPPORTED; }
+    
     void handle_optical_flow(const mavlink_message_t* msg);
 
     // vehicle-overridable message send function
@@ -843,6 +846,7 @@ private:
                                            const float roll,
                                            const float pitch,
                                            const float yaw);
+    void handle_landing_target(mavlink_message_t *msg);
 
     void lock_channel(mavlink_channel_t chan, bool lock);
 
