b4c3bebb7fd7b86340533caa3df5d5cf91b27c42
Andrew Tridgell <andrew@tridgell.net>
2019-06-01 13:50:07 +1000

    Copter: fixed missing override

--- ArduCopter/GCS_Mavlink.h
+++ ArduCopter/GCS_Mavlink.h
@@ -32,7 +32,7 @@ protected:
     MAV_RESULT handle_command_mount(const mavlink_command_long_t &packet) override;
     MAV_RESULT handle_command_int_packet(const mavlink_command_int_t &packet) override;
     MAV_RESULT handle_command_long_packet(const mavlink_command_long_t &packet) override;
-    MAV_RESULT handle_command_landing_target(const mavlink_landing_target_t &packet, uint32_t timestamp_ms);
+    MAV_RESULT handle_command_landing_target(const mavlink_landing_target_t &packet, uint32_t timestamp_ms) override;
 
     void handle_mount_message(const mavlink_message_t* msg) override;
 
